b75fd61b961cf1a48792be8a96acb83b230c9df9
Deeppanchal2108 <deepanchal2290@gmail.com>
2025-11-01 17:30:53 +0530

    made changes in the db

---
+++ packages/db/prisma/migrations/20251101115855/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Chat` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."Chat" DROP CONSTRAINT "Chat_roomId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."Chat" DROP CONSTRAINT "Chat_userId_fkey";
+
+-- DropTable
+DROP TABLE "public"."Chat";
+
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "type" TEXT NOT NULL,
+    "info" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "roomId" INTEGER NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
